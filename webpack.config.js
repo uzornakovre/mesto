303283d1fa300073bbe79960bbda361092925a66
Konstantin Filyaev <uzornakovre@yandex.ru>
2022-12-28 23:53:57 +0300

    feat: favicon added

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
   entry: { main: './src/pages/index.js' },
@@ -41,6 +42,11 @@ module.exports = {
       template: './src/index.html'
     }),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin(),
+    new FaviconsWebpackPlugin({
+      logo: './src/images/favicon.png',
+      mode: 'webapp',
+      devMode: 'webapp'
+    })
   ]
 }
